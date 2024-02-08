12ead9e7ced224269596f3cc05fbf4cd9670c45a
UJFord <ujford@gmail.com>
2024-02-08 10:01:38 +0800

    intializeing

---
+++ css/global-dec-bridge.php
@@ -0,0 +1,20 @@
+<style>
+    :root {
+        /* color pallete */
+        --mainBrand: #016A70;
+        --darkAccent: #647A78;
+        --lightAccent: #7FA9AD;
+        --darkShades: #202A21;
+        --lightShades: #FFFFDD;
+
+        /* others */
+        --searchTextColor: #e7e7e7;
+        --headColor: #ffc861;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
