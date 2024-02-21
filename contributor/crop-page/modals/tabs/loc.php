85aac6a31253ca7d38c9d1b7059b8995541e8fce
UJFord <ujford@gmail.com>
2024-02-22 00:27:42 +0800

    changed maptiler to openstreetmaps

--- contributor/crop-page/modals/tabs/loc.php
+++ contributor/crop-page/modals/tabs/loc.php
@@ -6,33 +6,31 @@
         aspect-ratio: 1/1;
     }
 </style>
-<!-- <style>
-    #map {
-        aspect-ratio: 1/1;
-    }
-</style> -->
 
 <div class="fade show active tab-pane" id="loc-tab-pane" role="tabpanel" aria-labelledby="loc-tab" tabindex="0">
 
     <div class="row">
 
-        <div class="col-6 border">
+        <div class="col-6 ">
 
         </div>
 
-        <div id="map" class="col border rounded">
+        <div id="map" class="col rounded">
         </div>
     </div>
 
 </div>
 
 <script>
-    const key = 'nEVXtMGf5Q4NUT81g14m';
-    const map = L.map('map').setView([6.1536, 124.993086], 9); //starting position
-    L.tileLayer(`https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=${key}`, { //style URL
+    // map tiler api key
+    // const key = 'nEVXtMGf5Q4NUT81g14m';
+    const map = L.map('map').setView([6.1536, 124.953086], 10); //starting position
+    L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, { //style URL
         tileSize: 512,
+        maxZoom: 16,
         zoomOffset: -1,
-        minZoom: 1,
+        minZoom: 10,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         crossOrigin: true
     }).addTo(map);
 </script>
