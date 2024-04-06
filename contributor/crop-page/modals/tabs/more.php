953f2d88832e301b407820fb677763692aee9202
NoobGaming69 <noel.salazar15.es@gmail.com>
2024-04-06 18:17:12 +0800

    added rice morph traits

--- contributor/crop-page/modals/tabs/more.php
+++ contributor/crop-page/modals/tabs/more.php
@@ -1,70 +1,33 @@
 <!-- MORE TAB -->
-<div class="fade tab-pane" id="more-tab-pane" role="tabpanel" aria-labelledby="more-tab" tabindex="0">
-    <!-- Cultural and Spiritual Significance-->
+<div class="fade show active tab-pane" id="more-tab-pane" role="tabpanel" aria-labelledby="more-tab" tabindex="0">
+    <!-- rice morphological traits -->
+    <?php require "crop-page/modals/tabs/rice-traits.php" ?>
+
+    <!-- Utilization ang Cultural Importance-->
     <div class="row mb-4">
-        <!-- Cultural-Significance -->
+        <label class="form-label"><strong>Utilization ang Cultural Importance</strong></label>
+        <!-- Significance -->
         <div class="col-12 mb-2">
-            <label for="Cultural-Significance" class="form-label small-font">Cultural-Significance</label>
-            <textarea name="cultural_significance" id="Cultural-Significance" cols="30" rows="2" class="form-control"></textarea>
+            <label for="Significance" class="form-label small-font">Significance</label>
+            <textarea name="significance" id="Significance" cols="30" rows="1" class="form-control"></textarea>
         </div>
 
-        <!-- Spiritual-Significance -->
+        <!-- Use -->
         <div class="col-12 mb-2">
-            <label for="Spiritual-Significance" class="form-label small-font">Spiritual-Significance</label>
-            <textarea name="spiritual_significance" id="Spiritual-Significance" cols="30" rows="2" class="form-control"></textarea>
+            <label for="Use" class="form-label small-font">Use</label>
+            <textarea name="use" id="Use" cols="30" rows="1" class="form-control"></textarea>
         </div>
 
-        <!-- cultural_importance -->
+        <!-- Indigenous Utilization -->
         <div class="col-12 mb-2">
-            <label for="Cultural-Importance" class="form-label small-font">Cultural Importance</label>
-            <textarea name="cultural_importance" id="Cultural-Importance" cols="30" rows="2" class="form-control"></textarea>
+            <label for="indigenous-utilization" class="form-label small-font">Indigenous Utilization</label>
+            <textarea name="indigenous_utilization" id="indigenous-utilization" cols="30" rows="2" class="form-control"></textarea>
         </div>
 
-        <!-- cultural_use-->
+        <!-- Remarkable Features-->
         <div class="col-12 mb-2">
-            <label for="Cultural-Use" class="form-label small-font">Cultural Use</label>
-            <textarea name="cultural_use" id="Cultural-Use" cols="30" rows="2" class="form-control"></textarea>
-        </div>
-    </div>
-
-    <br>
-    <hr>
-    <div id="more-help" class="form-text mb-2" style="font-size: 0.6rem;">the data below is based on farmers knowledge.</div>
-    <br>
-
-    <!-- Taste and Aroma-->
-    <div class="row mb-4">
-        <!-- Taste -->
-        <div class="col-4">
-            <label for="Taste" class="form-label small-font">Taste</label>
-            <input id="Taste" type="text" name="taste" class="form-control">
-        </div>
-
-        <!-- Aroma -->
-        <div class="col-4">
-            <label for="Aroma" class="form-label small-font">Aroma</label>
-            <input id="Aroma" type="text" name="aroma" class="form-control">
-        </div>
-
-        <!-- Maturation -->
-        <div class="col-4">
-            <label for="Maturation" class="form-label small-font">Maturation</label>
-            <input id="Maturation" type="text" name="maturation" class="form-control">
-        </div>
-    </div>
-
-    <!-- Pest and Disease -->
-    <div class="row mb-4">
-        <!-- Pest -->
-        <div class="col-6">
-            <label for="Pest" class="form-label small-font">Pest Resistance</label>
-            <input id="Pest" type="text" name="pest" class="form-control">
-        </div>
-
-        <!-- Disease -->
-        <div class="col-6">
-            <label for="Disease" class="form-label small-font">Disease Resistance</label>
-            <input id="Disease" type="text" name="diseases" class="form-control">
+            <label for="remarkable-features" class="form-label small-font">Remarkable Features</label>
+            <textarea name="remarkable_features" id="remarkable-features" cols="30" rows="2" class="form-control"></textarea>
         </div>
     </div>
 
