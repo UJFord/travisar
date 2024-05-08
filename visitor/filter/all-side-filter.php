e0ebb50064823b0a135303f34e390374dfda1911
UJFord <ujford@gmail.com>
2024-05-08 17:21:38 +0800

    fix overflow-x-scroll of all crop side filter

--- visitor/filter/all-side-filter.php
+++ visitor/filter/all-side-filter.php
@@ -8,7 +8,7 @@
     <div id="side-filter" class=" d-flex flex-column rounded border overflow-y-auto p-0">
 
         <!-- title -->
-        <div class="border-bottom d-flex align-items-center w-100 py-1 px-3 bg-light">
+        <div class="border-bottom d-flex align-items-center  py-1 px-3 bg-light">
             <h6 class="fw-semibold m-0 me-auto small-font">FILTERS</h6>
             <!-- help -->
             <a href="#" class="">
@@ -25,7 +25,7 @@
         </div>
 
         <!-- all crops -->
-        <div class="py-2 px-3 w-100 border-bottom">
+        <div class="py-2 px-3">
             <div id="crop-filter-dropdown-toggler" class="row d-flex align-items-center text-decoration-none text-dark" data-bs-toggle="collapse" href="#crop-filters" role="button" aria-expanded="true" aria-controls="crop-filters">
                 <i id="cropChev" class="chevron-dropdown-btn fas fa-chevron-down text-dark text-center col-1 rotate-chevron"></i>
                 <a class="fw-bold text-success col text-decoration-none" href="">Crops</a>
@@ -39,7 +39,7 @@
                 while ($row = pg_fetch_array($query_run)) {
             ?>
                     <!-- crops filters -->
-                    <div id="crop-filters" class="collapse w-100 mb-2">
+                    <div id="crop-filters" class="collapse  ps-4 mb-2">
                         <input class="form-check-input crop-filter" type="checkbox" id="category<?= $row['category_id']; ?>" value="<?= $row['category_id']; ?>">
                         <label for="category<?= $row['category_id']; ?>"><?= $row['category_name']; ?></label>
                     </div>
@@ -52,20 +52,20 @@
         </div>
 
         <!-- Varieties -->
-        <div class="py-2 px-3 w-100 border-bottom" id="variety-div">
+        <div class="py-2 px-3" id="variety-div">
             <div id="variety-filter-dropdown-toggler" class="row d-flex align-items-center text-decoration-none text-dark" data-bs-toggle="collapse" href="#variety-filters" role="button" aria-expanded="true" aria-controls="variety-filters">
                 <i id="varietyChev" class="chevron-dropdown-btn fas fa-chevron-down text-dark text-center col-1 rotate-chevron"></i>
                 <a class="fw-bold text-success col text-decoration-none" href="">Variety</a>
             </div>
 
             <!-- crops filters -->
-            <div id="variety-filters" class="collapse w-100 mb-2">
+            <div id="variety-filters" class="collapse  mb-2">
 
             </div>
         </div>
 
         <!-- terrain -->
-        <div class="py-2 px-3 border-bottom">
+        <div class="py-2 px-3">
             <div id="terrain-filter-dropdown-toggler" class="row d-flex align-items-center text-decoration-none text-dark" data-bs-toggle="collapse" href="#terrain-filters" role="button" aria-expanded="true" aria-controls="terrain-filters">
                 <i id="terrainChev" class="chevron-dropdown-btn fas fa-chevron-down text-dark text-center col-1 rotate-chevron"></i>
                 <a class="fw-bold text-success col text-decoration-none" href="">All Terrains</a>
@@ -80,7 +80,7 @@
                     while ($row = pg_fetch_array($query_run)) {
                 ?>
                         <!-- terrains filters -->
-                        <div id="terrain-filters" class="collapse w-100 mb-2">
+                        <div id="terrain-filters" class="collapse ps-4 mb-2">
                             <input class="form-check-input terrain-filter" type="checkbox" id="terrain<?= $row['terrain_id']; ?>" value="<?= $row['terrain_id']; ?>">
                             <label for="terrain<?= $row['terrain_id']; ?>"><?= $row['terrain_name']; ?></label>
                         </div>
@@ -94,12 +94,12 @@
         </div>
 
         <!-- all municipalities -->
-        <div class="pt-2 pb-1 px-3 border-bottom">
+        <div class="pt-2 pb-1 px-3">
             <div id="mun-filter-dropdown-toggler" class="row d-flex align-items-center text-decoration-none text-dark" data-bs-toggle="collapse" href="#municipality-filters" role="button" aria-expanded="true" aria-controls="municipalty-filters">
                 <i id="munChev" class="chevron-dropdown-btn fas fa-chevron-down text-dark col-1 rotate-chevron"></i>
                 <a class="fw-bold text-success col text-decoration-none" href="">All Municipalities</a>
             </div>
-            <div id="municipality-filters" class="collapse w-100 mb-2">
+            <div id="municipality-filters" class="collapse mb-2">
                 <?php
                 $query = "SELECT * FROM municipality order by municipality_name ASC";
                 $query_run = pg_query($conn, $query);
@@ -107,7 +107,7 @@
                 if ($query_run) {
                     while ($row = pg_fetch_array($query_run)) {
                 ?>
-                        <div id="municipality-filters" class="collapse w-100 mb-2">
+                        <div id="municipality-filters" class="collapse ps-4 mb-2">
                             <input class="form-check-input municipality-filter" type="checkbox" id="municipality<?= $row['municipality_id']; ?>" value="<?= $row['municipality_id']; ?>">
                             <label for="municipality<?= $row['municipality_id']; ?>"><?= $row['municipality_name']; ?></label>
                         </div>
@@ -121,23 +121,21 @@
         </div>
 
         <!-- all barangay -->
-        <div class="pt-2 pb-1 px-3 border-bottom" id="barangay-div">
+        <div class="pt-2 pb-1 px-3" id="barangay-div">
             <div id="brgy-filter-dropdown-toggler" class="row d-flex align-items-center text-decoration-none text-dark" data-bs-toggle="collapse" href="#brgy-filters" role="button" aria-expanded="true" aria-controls="brgy-filters">
                 <i id="brgyChev" class="chevron-dropdown-btn fas fa-chevron-down text-dark col-1 rotate-chevron"></i>
                 <a class="fw-bold text-success col text-decoration-none" href="">Barangay</a>
             </div>
-            <div id="brgy-filters" class="collapse w-100 mb-2">
+            <div id="brgy-filters" class="collapse ps-4 mb-2">
             </div>
         </div>
 
         <!-- button to submit filter -->
-        <div class="py-3 px-3 row border-bottom">
-            <div class="col">
-                <!-- Add a clear button -->
-                <button id="clearButton" class="btn btn-secondary hidden" onclick="clearSearch()">Clear</button>
-            </div>
-            <div class="input-group flex-row-reverse col">
-                <button id="searchButton" class="btn btn-success fw-semibold" onclick="applyFilters()"><i class="fa-solid fa-filter me-1 small-font"></i>Filter</button>
+        <div class="py-2 row border-bottom m-0">
+            <!-- Add a clear button -->
+            <div class="d-flex justify-content-end col">
+                <button id="clearButton" class="btn btn-link link-dark hidden me-3" onclick="clearSearch()">Clear<i class="fa-solid fa-eraser ms-1"></i></button>
+                <button id="searchButton" class="btn btn-success fw-semibold" onclick="applyFilters()">Filter<i class="fa-solid fa-filter small-font ms-1"></i></button>
             </div>
         </div>
     </div>
@@ -164,7 +162,7 @@
                     // Populate options
                     data.forEach(barangay => {
                         barangayFilter.innerHTML += `
-                        <div class="collapse show w-100 mb-2">
+                        <div class="collapse show  mb-2">
                             <input class="form-check-input brgy-filter" type="checkbox" id="barangay${barangay.barangay_id}" value="${barangay.barangay_id}">
                             <label for="barangay${barangay.barangay_id}">${barangay.barangay_name}</label>
                         </div>
@@ -224,7 +222,7 @@
                     // Populate options
                     data.forEach(variety => {
                         varietyFilter.innerHTML += `
-                        <div class="collapse show w-100 mb-2">
+                        <div class="collapse show  mb-2">
                             <input class="form-check-input variety-filter" type="checkbox" id="category_variety${variety.category_variety_id}" value="${variety.category_variety_id}">
                             <label for="category_variety${variety.category_variety_id}">${variety.category_variety_name}</label>
                         </div>
