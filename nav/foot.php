ffe4f76effde959eb953e853ed959bf7d22dd0d5
UJFord <ujford@gmail.com>
2024-05-09 19:31:43 +0800

    fix direciton of foot links

--- nav/foot.php
+++ nav/foot.php
@@ -28,10 +28,10 @@
                 <h4 class="mb-3 footer-head fw-bold">LINKS</h5>
                     <ul class="list-unstyled">
                         <li>
-                            <a href="<?= BASE_URL . '/' . 'visitor/about/crop.php'; ?>" class="link-light link-underline link-underline-opacity-0 link-opacity-50-hover">Crops</a>
+                            <a href="<?= BASE_URL . '/' . 'visitor/crop.php'; ?>" class="link-light link-underline link-underline-opacity-0 link-opacity-50-hover">Crops</a>
                         </li>
                         <li>
-                            <a href="<?= BASE_URL . '/' . 'visitor/about/crop.php'; ?>" class="link-light link-underline link-underline-opacity-0 link-opacity-50-hover">Map</a>
+                            <a href="<?= BASE_URL . '/' . 'visitor/crop.php'; ?>" class="link-light link-underline link-underline-opacity-0 link-opacity-50-hover">Map</a>
                         </li>
                         <li>
                             <a href="<?= BASE_URL . '/' . 'login/login.php'; ?>" class="link-light link-underline link-underline-opacity-0 link-opacity-50-hover">Contribute</a>
